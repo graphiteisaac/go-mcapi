b1d958caa2339fbefcf8a7ff45364441193ae9ea
isaac <mrminer2229.galaxycraft@gmail.com>
2022-08-01 16:13:46 +1000

    improve IP parsing for less (but not 0) errors

--- internal/util/parseIP.go
+++ internal/util/parseIP.go
@@ -3,6 +3,8 @@ package util
 import (
 	"fmt"
 	"net"
+	"strconv"
+	"strings"
 )
 
 type MinecraftAddress struct {
@@ -12,10 +14,23 @@ type MinecraftAddress struct {
 }
 
 func ParseIP(raw string) (addr MinecraftAddress, err error) {
+	fmt.Println("parsing IP....")
 	fmt.Println(net.ParseIP(raw))
-	addr.IP = raw
-	addr.Port = 25565
-	addr.Combined = fmt.Sprintf("%s:%d", addr.IP, addr.Port)
 
+	if strings.Contains(raw, ":") {
+		split := strings.Split(raw, ":")
+		port, err := strconv.Atoi(split[1])
+		if err != nil {
+			return addr, err
+		}
+
+		addr.IP = split[0]
+		addr.Port = uint16(port)
+	} else {
+		addr.IP = raw
+		addr.Port = 25565
+	}
+
+	addr.Combined = fmt.Sprintf("%s:%d", addr.IP, addr.Port)
 	return
 }
